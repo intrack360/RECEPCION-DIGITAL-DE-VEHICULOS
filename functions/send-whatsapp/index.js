a1a31334b7470b8b0db80ad5d5a6ed26f6e3b73b
MANUEL STAZ <staz01@hotmail.com>
2025-04-27 00:17:21 -0700

    Create index.js

---
+++ functions/send-whatsapp/index.js
@@ -0,0 +1,30 @@
+const functions=require('firebase-functions/v2/https');
+const axios=require('axios');
+const TOKEN=functions.config().waba.token;      //  firebase functions:config:set waba.token="EAA..."
+const PHONE_ID=functions.config().waba.phone_id;//  ... waba.phone_id="1234567890"
+
+exports.sendWhatsApp=functions.onRequest(async(req,res)=>{
+  const{to,folio,link}=req.body;
+  try{
+    await axios.post(
+      `https://graph.facebook.com/v19.0/${PHONE_ID}/messages`,
+      {
+        messaging_product:"whatsapp",
+        to:`whatsapp:+${to}`,
+        type:"template",
+        template:{
+          name:"contrato_firmado",
+          language:{code:"es_MX"},
+          components:[{type:"body",parameters:[
+            {type:"text",text:folio},
+            {type:"text",text:link}
+          ]}]
+        }
+      },
+      {headers:{Authorization:`Bearer ${TOKEN}`}}
+    );
+    res.status(200).send('ok');
+  }catch(e){
+    console.error(e.response?.data||e);res.status(500).send('error');
+  }
+});
