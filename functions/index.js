0a419cdf06970dc1b88a054d884a8cb25e8d4d6d
MANUEL STAZ <staz01@hotmail.com>
2025-04-27 11:17:53 -0700

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,39 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions/v2/https');
+const axios = require('axios');
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("¡Hola mundo desde Firebase!");
+const TOKEN = functions.config().waba.token;
+const PHONE_ID = functions.config().waba.phone_id;
+
+exports.sendWhatsapp = functions.onRequest(async (req, res) => {
+  const { to, folio, link } = req.body;
+  try {
+    await axios.post(
+      `https://graph.facebook.com/v19.0/${PHONE_ID}/messages`,
+      {
+        messaging_product: 'whatsapp',
+        to: `whatsapp:${to}`,
+        type: 'template',
+        template: {
+          name: 'contrato_firmado',
+          language: { code: 'es_MX' },
+          components: [
+            {
+              type: 'body',
+              parameters: [
+                { type: 'text', text: folio },
+                { type: 'text', text: link }
+              ]
+            }
+          ]
+        }
+      },
+      {
+        headers: { Authorization: `Bearer ${TOKEN}` }
+      }
+    );
+    res.status(200).send('Mensaje enviado');
+  } catch (error) {
+    console.error('Error al enviar mensaje:', error);
+    res.status(500).send('Error al enviar mensaje');
+  }
 });
