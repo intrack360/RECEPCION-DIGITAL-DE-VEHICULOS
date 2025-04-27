9dbb1432d939ce893b3f42e447be5eaecd5e45eb
MANUEL STAZ <staz01@hotmail.com>
2025-04-27 02:51:03 -0700

    Update index.js

--- functions/send-whatsapp/index.js
+++ functions/send-whatsapp/index.js
@@ -1,30 +1,39 @@
-const functions=require('firebase-functions/v2/https');
-const axios=require('axios');
-const TOKEN=functions.config().waba.token;      //  firebase functions:config:set waba.token="EAA..."
-const PHONE_ID=functions.config().waba.phone_id;//  ... waba.phone_id="1234567890"
+const functions = require('firebase-functions/v2/https');
+const axios = require('axios');
 
-exports.sendWhatsApp=functions.onRequest(async(req,res)=>{
-  const{to,folio,link}=req.body;
-  try{
+const TOKEN = functions.config().waba.token;
+const PHONE_ID = functions.config().waba.phone_id;
+
+exports.sendWhatsapp = functions.onRequest(async (req, res) => {
+  const { to, folio, link } = req.body;
+  try {
     await axios.post(
       `https://graph.facebook.com/v19.0/${PHONE_ID}/messages`,
       {
-        messaging_product:"whatsapp",
-        to:`whatsapp:+${to}`,
-        type:"template",
-        template:{
-          name:"contrato_firmado",
-          language:{code:"es_MX"},
-          components:[{type:"body",parameters:[
-            {type:"text",text:folio},
-            {type:"text",text:link}
-          ]}]
+        messaging_product: 'whatsapp',
+        to: `whatsapp:+${to}`,
+        type: 'template',
+        template: {
+          name: 'contrato_firmado',
+          language: { code: 'es_MX' },
+          components: [
+            {
+              type: 'body',
+              parameters: [
+                { type: 'text', text: folio },
+                { type: 'text', text: link }
+              ]
+            }
+          ]
         }
       },
-      {headers:{Authorization:`Bearer ${TOKEN}`}}
+      {
+        headers: { Authorization: `Bearer ${TOKEN}` }
+      }
     );
     res.status(200).send('ok');
-  }catch(e){
-    console.error(e.response?.data||e);res.status(500).send('error');
+  } catch (e) {
+    console.error(e.response?.data || e);
+    res.status(500).send('error');
   }
 });
